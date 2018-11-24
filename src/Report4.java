7dbdb8c1709a55c23134faafe12114bad5a8debb
Kanon Miyazato <e185762@ie.u-ryukyu.ac.jp>
2018-11-24 19:50:43 +0900

    3st commit ステップ3の実装です。

--- src/Report4.java
+++ src/Report4.java
@@ -1,11 +1,6 @@
 public class Report4 {
     public static void main(String[] args) {
-        try{
-            String str = null;
-            System.out.println(str.length());
-        } catch (NullPointerException e){
-            System.out.println("例外:NullPointerExceptionが発生しました。");
-            System.out.println("エラー原因:"+e.getMessage());
-        }
+        String s = "百二十三";
+        int value = Integer.parseInt(s);
     }
 }
